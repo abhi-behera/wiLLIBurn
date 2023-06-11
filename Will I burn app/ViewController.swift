9dd6218ba5e6e3bb5b4a580b7d2b484f498c8dd0
Abhijit behera <abhijitbehera1997@gmail.com>
2023-06-11 12:29:48 +0530

    Initial Commit

---
+++ Will I burn app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Will I burn app
+//
+//  Created by Sritam on 11/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
