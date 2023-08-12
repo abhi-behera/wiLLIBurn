1e1ae6725f30e0e8053d70923aea7119e505433c
Abhijit behera <abhijitbehera1997@gmail.com>
2023-08-13 01:55:04 +0530

    layout done action bar done Location access alert done

---
+++ Will I burn app/SkinType.swift
@@ -0,0 +1,31 @@
+//
+//  SkinType.swift
+//  Will I burn app
+//
+//  Created by Sritam on 27/06/23.
+//
+
+import Foundation
+
+struct SkinType{
+    
+    let type1 = "Type 1 :  Pale / light type skin"
+    
+    let type2 = "Type 2 :  White / Fair type skin"
+    
+    let type3 = "Type 3 :  Medium type skin"
+    
+    let type4 = "Type 4 :  Olive brown type skin"
+    
+    let type5 = "Type 5 :  Dark brown type skin"
+    
+    let type6 = "Type 6 :  Very dark / black type skin"
+    
+    
+    func allSkinType () -> [String]{
+        
+        return [type1, type2, type3, type4, type5, type6]
+        
+    }
+
+}
--- Will I burn app/ViewController.swift
+++ Will I burn app/ViewController.swift
@@ -6,14 +6,65 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    var locationManager = CLLocationManager()
+    
+    @IBOutlet weak var skintypLable: UILabel!
+    
+    @IBOutlet weak var time: UILabel!
+    
+    var skintyp = " "
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization() //  this will call the below func.s
     }
 
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        print (" Auth changed ")
+        if manager.authorizationStatus == .authorizedWhenInUse{
+            getLoc()
+        }
+        else{
+            print("location denied")
+            let alert = UIAlertController(title: "Hello !", message: "Pls enable location", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default,handler: nil))
+            self.present(alert, animated: false,completion:  getLoc)
+        }
+    }
 
+    func getLoc ()
+    {
+        
+    }
+    
+    @IBAction func skintypChange(_ sender: Any) {
+        
+        let alert1 = UIAlertController(title: "Pick one!", message: "Pls choose your skin type", preferredStyle: .actionSheet)
+        for s in SkinType().allSkinType(){
+            alert1.addAction(UIAlertAction(title: s, style: .default, handler: {( action) in
+                self.skintyp = s
+                self.skintypeLableUpdfate()
+            }))
+        }
+        self.present(alert1, animated: true)
+        
+    }
+    
+    @IBAction func setRemdBtn(_ sender: Any) {
+        
+        
+    }
+    
+    func skintypeLableUpdfate(){
+        skintypLable.text = skintyp
+    }
+    
+    
+    
 }
 
